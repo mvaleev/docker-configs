7dc41815261bf5fbd53ec741903f81e547e7a66a
Maxim <mvaleev@users.noreply.github.com>
2017-05-04 00:30:54 +0300

    add elk and sentry

---
+++ elk/elk-elastalert/start-elastalert.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+# Wait until Elasticsearch is online since otherwise Elastalert will fail.
+rm -f garbage_file
+while ! wget -O garbage_file ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT} 2>/dev/null
+do
+	echo "Waiting for Elasticsearch..."
+	rm -f garbage_file
+	sleep 1
+done
+rm -f garbage_file
+sleep 5
+
+# Check if the Elastalert index exists in Elasticsearch and create it if it does not.
+if ! wget -O garbage_file ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/elastalert_status 2>/dev/null
+then
+	echo "Creating Elastalert index in Elasticsearch..."
+    elastalert-create-index --host ${ELASTICSEARCH_HOST} --port ${ELASTICSEARCH_PORT} --config ${ELASTALERT_CONFIG} --index elastalert_status --old-index ""
+else
+    echo "Elastalert index already exists in Elasticsearch."
+fi
+rm -f garbage_file
+
+echo "Starting Elastalert..."
+exec python -m elastalert.elastalert --config ${ELASTALERT_CONFIG} --verbose
